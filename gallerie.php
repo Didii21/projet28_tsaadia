e4321b2acbc4f6bff47f111cf008aa859787a50e
Didii21 <140284548+Didii21@users.noreply.github.com>
2023-12-02 15:32:38 +0100

    fin

---
+++ gallerie.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>sites</title>
+    
+</head>
+<div class="content">
+<h1>Notre galerie d images</h1>
+</div>
+<?php include ("navbar.php") ?>
+<h2>Allons a Ouagadougou</h2>
+<div class="photos">
+    <img src="" alt="">
+</div>
+<h2>Ici c'est Banfora</h2>
+<div class="photos">
+    <img src="" alt="">
+</div>
+<h2>Dédougou c'est par là</h2>
+<div class="photos">
+    <img src="" alt="">
+</div>
+<!-- <header>
+    <h2> Africulture !!</h2>
+</header>
+<body>
+    <h2>Les crocodiles sacrés de Sabou</h2>
+    <h2>Les gravures rupestres du Sahel</h2>
+    <h2>La marre aux hyppopotames de Bala</h2>
+</body> -->
+<?php include ("footer.php") ?>
+</html>
